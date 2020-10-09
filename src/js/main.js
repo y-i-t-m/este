e78669341e37d24e7c03ffd8720abe646753e8f1
y-i-t-m <>
2020-10-09 17:58:45 +0900

    FontAwesomeをnpm経由で読み込み

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 import "../scss/style.scss";
 
-import "../img/favicon.png";
-import "../img/webpack-logo.svg";
+import {dom, library} from '@fortawesome/fontawesome-svg-core';
+import {faDog, faCat,faChevronUp} from '@fortawesome/free-solid-svg-icons';
+import {faComments} from '@fortawesome/free-regular-svg-icons';
+
+
+// 使いたいFontAwesomeアイコンをimport文 => {} 中に記述してください。
+// その後,library.add内にも記述し、htmlに<i>タグで記述してください。
+library.add(faDog,faComments,faCat,faChevronUp);
+
+dom.i2svg();
